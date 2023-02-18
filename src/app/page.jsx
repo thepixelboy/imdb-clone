1c3db3ea9fb4cd0ac4733b5e60ab7a0c4cecf72b
Ruben <34570952+thepixelboy@users.noreply.github.com>
2023-02-18 19:44:09 +0100

    feat: install tailwindcss

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,5 +4,5 @@ import { Inter } from "@next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <h1>Home</h1>;
+  return <h1 className="text-red-600 text-3xl font-bold">Home</h1>;
 }
