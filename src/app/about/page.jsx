0dac909d87e7448859e803467c8d2ad3ba2ac925
Ruben <34570952+thepixelboy@users.noreply.github.com>
2023-02-19 16:04:05 +0100

    feat: create about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,49 @@
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About Us</h1>
+      <p>
+        Welcome to our movie database, the ultimate online resource for all your
+        movie-related needs!
+      </p>
+      <p>
+        Our database is the go-to destination for movie enthusiasts all around
+        the world, providing a comprehensive collection of information on a vast
+        array of films accross the globe.
+      </p>
+      <p>
+        We have been passionately curating our database for many years now,
+        constantly updating and expanding it to provide our users with the most
+        up-to-date and relevant information possible. Our team of dedicated
+        movie experts works tirelessly to ensure that every film in our database
+        is accurately cataloged, with details on the cast, crew, plot, genre,
+        release date, and much more.
+      </p>
+      <p>
+        At our core, we are commited to making it easy for our users to discover
+        new movies, explore their favorite genres, and stay up-to-date on the
+        latest releases. Whether you&apos;re a die-hard cinephile or just
+        looking for something fun to watch on Friday noght, we have everything
+        you need to make your movie experience unforgettable.
+      </p>
+      <p>
+        We are passionate about movies, and we&apos;re commited to shating that
+        passion with you. So, whether you&apos;re a fan of classic films or the
+        latest blockbusters, join us and start exploring our movie database
+        today!
+      </p>
+      <p className="text-xs mt-5">
+        Note: This page&apos;s text was generated using{" "}
+        <a
+          className="underline"
+          target="_blank"
+          rel="noreferrer"
+          href="https://chat.openai.com"
+        >
+          ChatGPT
+        </a>
+        .
+      </p>
+    </div>
+  );
+}
